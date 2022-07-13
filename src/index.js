0d68395ef3efc3865538a5448bbb33758e201aaf
OlesiaOk <48320001+OlesiaOk@users.noreply.github.com>
2022-07-13 17:29:19 +0300

    add function "formatDay", add dayly forecasty

--- src/index.js
+++ src/index.js
@@ -33,20 +33,35 @@ function reciveTime() {
 let currentDate = document.querySelector("#date");
 currentDate.innerHTML = reciveTime();
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
 function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
         <div class="col-2">
-          <div class="weather-forecast-date">${day}</div>
-          <div class="weather-forecast-temperature"><span class="temperature-max">20℃</span> <span class="temperature-min">12℃</span></div>
-          <div class="weather-forecast-icon"><img src=""  alt="Clear" width="42" id=""/></div>
+          <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+          <div class="weather-forecast-temperature"><span class="temperature-max">${Math.round(
+            forecastDay.temp.max
+          )}℃</span> <span class="temperature-min">${Math.round(
+          forecastDay.temp.min
+        )}℃</span></div>
+          <div class="weather-forecast-icon"><img src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"  alt="Clear" width="60" id=""/></div>
                    
          </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
